795ec02155e6ece1f6cf150fc1fc8ca73296e0ab
Sohan Madhana <54635473+sohanmadhana@users.noreply.github.com>
2024-08-23 19:18:55 +0000

    fundamentals-statement-and-variables

---
+++ index.js
@@ -0,0 +1,22 @@
+// Statement is piece of code which expresses an action to be carried out.
+
+// Variable
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number eg: 1name
+// Cannot contain a space or hyphen
+// variables are case-sensitive
+
+let name = 'Sohan';
+console.log(name);
+
+const interestRate = 0.3;
+// interestRate = 1;
+console.log(interestRate)
+
+// 'let' is used when the variable may be reassigned
+// 'const' is used when the variable value is fixed.
+
+let firstName = 'Sohan'; //String literal
+let age = 30; //number literal
+let isApproved = true; //boolean literal
